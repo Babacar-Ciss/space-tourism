e5bc6b3df239e2a24e5b9e2721b2505b9574e382
kratos nvidia <kratosnvidia@gmail.com>
2023-02-04 01:41:22 +0000

    initialisation styled component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,20 @@
-"use client"
-
-import './globals.css'
-import styled from 'styled-components'
-
-
-const P = styled.p`
-  color: aquamarine;
-`
+import { GlobalStyle } from "@/styles/globalStyle";
+import StyledComponentsRegistry from "./lib/registry";
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html lang="en">
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>
-        <P>Babacar Ciss
-          
-        </P>
-        {children}
-        
-        
-        
-        
-        </body>
-    </html>
-  )
+			<head />
+			<body>
+				<StyledComponentsRegistry>
+					<GlobalStyle />
+					{children}
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	);
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const HeaderStyle = styled.header``;
+
+const Header = () => {
+	return <div></div>;
+};
+
+export default Header;
